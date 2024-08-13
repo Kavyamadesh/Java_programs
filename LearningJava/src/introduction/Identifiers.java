7277357aa042c0ac2b58122ba4688002f5e50823
Kavyamadesh <kavyamadesh06@gmail.com>
2024-08-13 15:16:19 +0530

    identifier program has been added to the project

---
+++ LearningJava/src/introduction/Identifiers.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class Identifiers {
+
+	public static void main(String[] args)
+	{
+		// identifiers are the names give to our programs or variables. 
+		int      a         = 10;
+	//datatype indentifier = value
+		
+		System.out.println(a);
+
+	}
+
+}
--- LearningJava/src/introduction/Introduction.java
+++ LearningJava/src/introduction/Introduction.java
@@ -2,10 +2,8 @@ package introduction;
 
 public class Introduction 
 {
-
 	public static void main(String[] args) 
 	{
-		
       System.out.println("HELLO WORLD");
 	}
 
